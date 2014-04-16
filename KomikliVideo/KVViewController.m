8d361e102b29e107136c40bace1e278d4b588bc7
Can Bülbül <alicanblbl@gmail.com>
2014-04-16 13:28:02 +0300

    seen fix

--- KomikliVideo/KVViewController.m
+++ KomikliVideo/KVViewController.m
@@ -135,8 +135,7 @@ blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 }
 
 - (void)playVideoWithId:(NSString *)videoId {
-    /*NSString *html = [NSString stringWithFormat:vidBase, (int)player.frame.size.width, (int)player.frame.size.height, videoId];
-    [player loadHTMLString:html baseURL:[NSURL URLWithString:@"http://youtube.com"]];*/
+    [NSString stringWithContentsOfURL:[NSURL URLWithString:f(@"http://hgeg.io/komiktv/watched/%@/%@/",uid,videoId)] encoding:NSUTF8StringEncoding error: nil];
     XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [[XCDYouTubeVideoPlayerViewController alloc] initWithVideoIdentifier:videoId];
     [videoPlayerViewController presentInView:self.videoContainerView];
     [videoPlayerViewController.moviePlayer play];
